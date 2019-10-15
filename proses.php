4bbba8c98b488fc7e7deaad803dfbde2d17216ad
aisha rahmayanti <55432540+aisharahmayantii@users.noreply.github.com>
2019-10-15 15:54:28 +0700

    Add files via upload
    Tugas 6

---
+++ proses.php
@@ -0,0 +1,13 @@
+<!--Menampilkan nilai form HTML ke dalam PHP -->
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo $_GET['nama'];
+echo "<br />";
+echo $_GET['email'];
+?>
+
+</body>
+</html>
---
+++ variable.php
@@ -0,0 +1,16 @@
+<!--Aturan Penulisan Variabel dalam PHP-->
+<!--Penulisan variabel harus diawali dengan tanda $ Variabel di dalam PHP harus diawali dengan dollar sign atau tanda dollar ($)-->
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$i;
+$nama;
+$Umur;
+$_lokasi_memori;
+$ANGKA_MAKSIMUM;
+?>
+
+</body>
+</html>
